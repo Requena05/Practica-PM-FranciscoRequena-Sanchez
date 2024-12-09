6234ab41439bbaa02bfcf7c7aa84f64197ec4fea
franr <franreke@gmail.com>
2024-12-09 20:24:43 +0100

    duro

--- app/src/main/java/com/example/practica_pm_franciscorequena_sanchez/MainActivity.kt
+++ app/src/main/java/com/example/practica_pm_franciscorequena_sanchez/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.practica_pm_franciscorequena_sanchez
 
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.lifecycle.coroutineScope
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenStarted
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 
@@ -22,8 +26,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var vida1: ImageView ;private lateinit var vida2: ImageView ;private lateinit var vida3: ImageView
     private lateinit var vidaperdida: ImageView ;private lateinit var vidaperdida2: ImageView ;private lateinit var vidaperdida3: ImageView
     private lateinit var reiniciar: AppCompatButton;private lateinit var vidas:LinearLayout
-
+    private var contenido=0
     private var idcarta=0
+    private var ispareja:Boolean?=false
+    private var vidaextra=0
     private val posicion=mutableListOf(1,1,2,2,3,3,4,4,5,5,6,6)
     private var cartasgiradas= mutableListOf(false,false,false,false,false,false,false,false,false,false,false,false)
     private var primera_carta_girada:Int?=null
@@ -50,77 +56,99 @@ class MainActivity : AppCompatActivity() {
 
         //Barajar nuestras cartas
         pareja(posicion)
-        carta1.setOnClickListener{ flipCard(carta1,posicion[0]);compararpareja();ganar() }
-        carta2.setOnClickListener{ flipCard(carta2,posicion[1]);compararpareja();ganar() }
-        carta3.setOnClickListener{flipCard(carta3,posicion[2]);compararpareja();ganar()}
-        ;carta4.setOnClickListener{flipCard(carta4,posicion[3]);compararpareja();ganar()}
-        carta5.setOnClickListener{flipCard(carta5,posicion[4]);compararpareja();ganar()}
-        carta6.setOnClickListener{flipCard(carta6,posicion[5]);compararpareja();ganar()}
-        carta7.setOnClickListener{flipCard(carta7,posicion[6]);compararpareja();ganar()}
-        carta8.setOnClickListener{flipCard(carta8,posicion[7]);compararpareja();ganar()}
-        carta9.setOnClickListener{flipCard(carta9,posicion[8]);compararpareja();ganar()}
-        carta10.setOnClickListener{flipCard(carta10,posicion[9]);compararpareja();ganar()}
-        carta11.setOnClickListener{flipCard(carta11,posicion[10]);compararpareja();ganar()}
-        carta12.setOnClickListener{flipCard(carta12,posicion[11]);compararpareja();ganar()}
+        carta1.setOnClickListener{contenido=0;compararpareja();ganar()}
+        carta2.setOnClickListener{contenido=1;compararpareja();ganar()}
+        carta3.setOnClickListener{contenido=2;compararpareja();ganar()}
+        ;carta4.setOnClickListener{contenido=3;compararpareja();ganar()}
+        carta5.setOnClickListener{contenido=4;compararpareja();ganar()}
+        carta6.setOnClickListener{contenido=5;compararpareja();ganar()}
+        carta7.setOnClickListener{contenido=6;compararpareja();ganar()}
+        carta8.setOnClickListener{contenido=7;compararpareja();ganar()}
+        carta9.setOnClickListener{contenido=8;compararpareja();ganar()}
+        carta10.setOnClickListener{contenido=9;compararpareja();ganar()}
+        carta11.setOnClickListener{contenido=10;compararpareja();ganar()}
+        carta12.setOnClickListener{contenido=11;compararpareja();ganar()}
+
+    }
+    override fun onResume() {
+        super.onResume()
+        SystemClock.sleep(1000)
     }
 
     fun pareja(barajas:MutableList<Int>){
         barajas.shuffle()}
 
-    private fun flipCardreverse(card: ImageView ,position:Int) {
+    private fun flipCardreverse(card: ImageView,position:Int) {
         idcarta=position
-        when(idcarta){
-            1->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta);card.scaleType=ImageView.ScaleType.FIT_XY}
-            2->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta);card.scaleType=ImageView.ScaleType.FIT_XY}
-            3->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta);card.scaleType=ImageView.ScaleType.FIT_XY}
-            4->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta);card.scaleType=ImageView.ScaleType.FIT_XY}
-            5->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta);card.scaleType=ImageView.ScaleType.FIT_XY}
-            6->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta);card.scaleType=ImageView.ScaleType.FIT_XY}
-        }
+        card.animate().rotationY(180F).setDuration(500).start();
+        card.setImageResource(R.drawable.carta);card.scaleType=ImageView.ScaleType.FIT_XY
+
+
     }
     private fun flipCard(card: ImageView,position:Int) {
         idcarta=position
+        if (card.rotationY == 0F){
+            when(idcarta){
+                1->{card.animate().rotationY(180F).setDuration(500).start();card.setImageResource(R.drawable.carta1);card.scaleType=ImageView.ScaleType.FIT_XY}
+                2->{card.animate().rotationY(180F).setDuration(500).start();card.setImageResource(R.drawable.carta2);card.scaleType=ImageView.ScaleType.FIT_XY}
+                3->{card.animate().rotationY(180F).setDuration(500).start();card.setImageResource(R.drawable.carta3);card.scaleType=ImageView.ScaleType.FIT_XY}
+                4->{card.animate().rotationY(180F).setDuration(500).start();card.setImageResource(R.drawable.carta4);card.scaleType=ImageView.ScaleType.FIT_XY}
+                5->{card.animate().rotationY(180F).setDuration(500).start();card.setImageResource(R.drawable.carta5);card.scaleType=ImageView.ScaleType.FIT_XY}
+                6->{card.animate().rotationY(180F).setDuration(500).start();card.setImageResource(R.drawable.carta6);card.scaleType=ImageView.ScaleType.FIT_XY}
+            }
+        }else{
         when(idcarta){
-            1->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta1);card.scaleType=ImageView.ScaleType.FIT_XY}
-            2->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta2);card.scaleType=ImageView.ScaleType.FIT_XY}
-            3->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta3);card.scaleType=ImageView.ScaleType.FIT_XY}
-            4->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta4);card.scaleType=ImageView.ScaleType.FIT_XY}
-            5->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta5);card.scaleType=ImageView.ScaleType.FIT_XY}
-            6->{card.animate().rotationY(360F).setDuration(1000).start();card.setImageResource(R.drawable.carta6);card.scaleType=ImageView.ScaleType.FIT_XY}
-        }
+            1->{card.animate().rotationY(360F).setDuration(500).start();card.setImageResource(R.drawable.carta1);card.scaleType=ImageView.ScaleType.FIT_XY}
+            2->{card.animate().rotationY(360F).setDuration(500).start();card.setImageResource(R.drawable.carta2);card.scaleType=ImageView.ScaleType.FIT_XY}
+            3->{card.animate().rotationY(360F).setDuration(500).start();card.setImageResource(R.drawable.carta3);card.scaleType=ImageView.ScaleType.FIT_XY}
+            4->{card.animate().rotationY(360F).setDuration(500).start();card.setImageResource(R.drawable.carta4);card.scaleType=ImageView.ScaleType.FIT_XY}
+            5->{card.animate().rotationY(360F).setDuration(500).start();card.setImageResource(R.drawable.carta5);card.scaleType=ImageView.ScaleType.FIT_XY}
+            6->{card.animate().rotationY(360F).setDuration(500).start();card.setImageResource(R.drawable.carta6);card.scaleType=ImageView.ScaleType.FIT_XY}
+        }}
     }
     private fun checkclick(){
         if(primera_carta_girada==null){
-            primera_carta_girada=posicion[idcarta]
+            primera_carta_girada=posicion[contenido]
         }else{
-            segunda_carta_girada=posicion[idcarta]
+            segunda_carta_girada=posicion[contenido]
         }
     }
 
     private fun compararpareja() {
         checkclick()
+        when(contenido){
+            0->flipCard(carta1,posicion[0]);1->flipCard(carta2,posicion[1]);2->flipCard(carta3,posicion[2])
+            3->flipCard(carta4,posicion[3]);4->flipCard(carta5,posicion[4]);5->flipCard(carta6,posicion[5])
+            6->flipCard(carta7,posicion[6]);7->flipCard(carta8,posicion[7]);8->flipCard(carta9,posicion[8])
+            9->flipCard(carta10,posicion[9]);10->flipCard(carta11,posicion[10]);11->flipCard(carta12,posicion[11])
+        }
        if(primera_carta_girada!=null && segunda_carta_girada!=null){
            if (primera_carta_girada!! == segunda_carta_girada!!) {
-               if(!cartasgiradas[primera_carta_girada!!] && !cartasgiradas[segunda_carta_girada!!]){
+               if(!cartasgiradas[primera_carta_girada!!] && !cartasgiradas[segunda_carta_girada!!]
+                   && cartasgiradas[primera_carta_girada!!]!=cartasgiradas[segunda_carta_girada!!]){
                    ganador++
+                   cartasgiradas[primera_carta_girada!!] =true
+                   cartasgiradas[segunda_carta_girada!!] =true
                    }
-               cartasgiradas[primera_carta_girada!!] =true
-               cartasgiradas[segunda_carta_girada!!] =true
-
                primera_carta_girada=null
                segunda_carta_girada=null
            }else{
-               fallos++
 
+               guardarcartas()
+               fallos++
                primera_carta_girada=null
                segunda_carta_girada=null
            }
        }
     }
 
+private fun guardarcartas(){
 
-
-
+    flipCardreverse(carta1,posicion[0]);flipCardreverse(carta2,posicion[1]);flipCardreverse(carta3,posicion[2])
+    flipCardreverse(carta4,posicion[3]);flipCardreverse(carta5,posicion[4]);flipCardreverse(carta6,posicion[5])
+    flipCardreverse(carta7,posicion[6]);flipCardreverse(carta8,posicion[7]);flipCardreverse(carta9,posicion[8])
+    flipCardreverse(carta10,posicion[9]);flipCardreverse(carta11,posicion[10]);flipCardreverse(carta12,posicion[11])
+}
 
 
 
@@ -131,10 +159,14 @@ class MainActivity : AppCompatActivity() {
             vidas.visibility=LinearLayout.GONE
             reiniciar.setOnClickListener{
                 recreate()
-            }}
+            }
+        }
         when(fallos){
-            1->{vidaperdida.visibility=ImageView.VISIBLE;vida1.visibility=ImageView.GONE}
-            2->{vidaperdida2.visibility=ImageView.VISIBLE;vida2.visibility=ImageView.GONE}
+
+            1->{vidaperdida.visibility=ImageView.VISIBLE;vida1.visibility=ImageView.GONE
+            }
+            2->{vidaperdida2.visibility=ImageView.VISIBLE;vida2.visibility=ImageView.GONE
+            }
             3->{
                 vidaperdida3.visibility=ImageView.VISIBLE;vida3.visibility=ImageView.GONE
                 Toast.makeText(this, "Has perdido", Toast.LENGTH_SHORT).show()
@@ -147,14 +179,14 @@ class MainActivity : AppCompatActivity() {
                 flipCardreverse(carta4,posicion[3]);flipCardreverse(carta5,posicion[4]);flipCardreverse(carta6,posicion[5])
                 flipCardreverse(carta7,posicion[6]);flipCardreverse(carta8,posicion[7]);flipCardreverse(carta9,posicion[8])
                 flipCardreverse(carta10,posicion[9]);flipCardreverse(carta11,posicion[10]);flipCardreverse(carta12,posicion[11])
-
-                carta1.setOnClickListener(null);carta2.setOnClickListener(null);carta3.setOnClickListener(null)
+                 carta1.setOnClickListener(null);carta2.setOnClickListener(null);carta3.setOnClickListener(null)
                 carta4.setOnClickListener(null);carta5.setOnClickListener(null);carta6.setOnClickListener(null)
                 carta7.setOnClickListener(null);carta8.setOnClickListener(null);carta9.setOnClickListener(null)
                 carta10.setOnClickListener(null);carta11.setOnClickListener(null);carta12.setOnClickListener(null)
             }
 
         }
+
     }
 }
 
